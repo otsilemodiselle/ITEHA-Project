e41d232c1c275b08cf09733fdc5291e0e5f39992
Otsile Modiselle <otsilemodiselle@outlook.com>
2024-08-24 16:07:06 +0200

    implement client side validation for create category page

--- Bulky/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ Bulky/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aba41ef27dc00e9990128b8fbc181d7ff113b1fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44413892532f5e2b0e5a3aa8730ad8210f3d8a76")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
