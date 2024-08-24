c26f920510d5dd60761ab27337c03f62db6a3640
Otsile Modiselle <otsilemodiselle@outlook.com>
2024-08-24 11:31:24 +0200

    update bootstrap theme and icons

--- Bulky/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ Bulky/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18347e2e842de346293b936b1591212203b1c6b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9ac9a906b3becd0f0ecb196cfa85c5b86faee4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
