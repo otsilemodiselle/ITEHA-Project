2f304484b797ffbb3fd3cec253b2a3c327bcbede
Otsile Modiselle <otsilemodiselle@outlook.com>
2024-08-26 21:32:46 +0200

    clean up main branch in anticipation of catch-up merge

--- Bulky/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ Bulky/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8420d8c91b62a883ccef56c64ccad7ce0e80a3ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e63fecf418e8f338f9a9b8690bdb006f07ff7bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
