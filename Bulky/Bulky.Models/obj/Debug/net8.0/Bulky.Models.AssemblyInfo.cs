26764d2022b87e74cc5d929bef6b4b3c96380d9e
Otsile Modiselle <otsilemodiselle@outlook.com>
2024-08-26 22:11:19 +0200

    clean up main branch

--- Bulky/Bulky.Models/obj/Debug/net8.0/Bulky.Models.AssemblyInfo.cs
+++ Bulky/Bulky.Models/obj/Debug/net8.0/Bulky.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bulky.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+054adee9ade9a96740a884523d20ece2003a4c55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+319c35e511d98f28a32725a334bba2b1dca20cef")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bulky.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bulky.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
