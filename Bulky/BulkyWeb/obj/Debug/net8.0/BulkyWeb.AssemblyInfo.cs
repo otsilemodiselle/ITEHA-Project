63e925b08d0204b574a7d4954bd5a423de73f6d0
Otsile Modiselle <otsilemodiselle@outlook.com>
2024-08-30 18:38:50 +0200

    create and action and view for the order summary

--- Bulky/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ Bulky/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44bddca0af0d49101cf69e51a3b5fcd9d768da3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81bad627bd5e651398b923a95f329ded897e825b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
